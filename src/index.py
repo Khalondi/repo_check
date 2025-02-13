ab5a699de85ac1a7653ff7ecdae0c45e0d4b3051
Khalondi <khalondivanessa@gmail.com>
2025-02-13 09:55:43 +0300

    FIX: Fixed the distance calculator

--- src/index.py
+++ src/index.py
@@ -4,21 +4,27 @@ import functools
 from functools import partial
 from geopy.geocoders import Nominatim
 from geopy import distance
-from geopy.distance import Distance
+from geopy.distance import geodesic
 
 geolocator = Nominatim(user_agent="Mickhie's Delivery App")
 
 geocode = partial(geolocator.geocode, language="en")
 user_location=input("Enter your location: ")
-print(geocode(user_location))
-location =geolocator.reverse("0.264913, 32.546467")
-print(location.address)
+user_location=geocode(user_location)
+print(f"You are in, {user_location}")
+location =geolocator.geocode("Bunamwaya")
 
 def distance_calculator(address1, address2):
-    address1=geocode(user_location)
-    address2=geocode(location)
-    distance=(geopy.distance.distance(address2,address1))
    
-    return distance
-print(f"The distance between our address and your location is{distance_calculator} kilometers.")
+    
+    if not address1 or not address2:
+        return "Could not resolve your location! Try refereshing and starting again."
+    
+    coords_address1 =(address1.latitude, address1.longitude)
+    coords_address2 = (address2.latitude, address2.longitude)
+    return geodesic(coords_address2, coords_address1).kilometers
+
+
+distance_result= distance_calculator(location, user_location) 
+print(f"The distance between our address and your location is{distance_result:2f} kilometers.")
    
